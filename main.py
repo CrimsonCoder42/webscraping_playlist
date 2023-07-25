248bf101f201396ccac0d2d24449b0dd1dac7825
CrimsonCoder42 <nostro37@gmail.com>
2023-07-25 13:57:26 -0400

    playlists are loading

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ sp = spotipy.Spotify(
     )
 )
 user_id = sp.current_user()["id"]
-print(user_id)
+
 
 #Searching Spotify for songs by title
 song_uris = []
 year = date.split("-")[0]
 for song in song_names:
     result = sp.search(q=f"track:{song} year:{year}", type="track")
-    print(result)
+
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
@@ -47,7 +47,7 @@ for song in song_names:
 
 #Creating a new private playlist in Spotify
 playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
-print(playlist)
+
 
 #Adding songs found into the new playlist
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
